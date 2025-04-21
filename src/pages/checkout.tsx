c7908485f7e88172525dc61b69e307ec706510c0
Cai <nicai.msn@gmail.com>
2025-04-22 09:48:13 +1000

    Cleanup

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -58,7 +58,6 @@ export default function Checkout() {
         const customer = mainContext.getCustomer();
         if (customer) {
             form.reset(customer);
-            
         }
         
         const storedPickupOrDelivery = mainContext.getPickupOrDelivery();
