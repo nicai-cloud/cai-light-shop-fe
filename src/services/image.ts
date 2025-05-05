14bc7e36ac6a7f74048795c76763f42b8f22c0da
Cai <nicai.msn@gmail.com>
2025-05-05 16:44:06 +1000

    Add types

--- src/services/image.ts
+++ src/services/image.ts
@@ -1,14 +1,14 @@
 export const getAllLightsImageUrls = async (): Promise<string[]> => {
     const response = await fetch(`${import.meta.env.VITE_LIGHT_SHOP_API}/images/lights`);
-    return await response.json();
+    return (await response.json()).images;
 }
 
 export const getLightImageUrlsByInternalName = async (internalName: string): Promise<string[]> => {
     const response = await fetch(`${import.meta.env.VITE_LIGHT_SHOP_API}/images/lights/${internalName}`);
-    return await response.json();
+    return (await response.json()).images;
 }
 
 export const getAllLightVariantsImageUrls = async (): Promise<string[]> => {
     const response = await fetch(`${import.meta.env.VITE_LIGHT_SHOP_API}/images/light-variants`);
-    return await response.json();
+    return (await response.json()).images;
 }
